0b63e4d3d8f83524a317fd0ea8fdfcc533ef2d54
jinbanglin <17golang@gmail.com>
2018-09-17 16:36:59 +0800

    update

--- client/rpc_client.go
+++ client/rpc_client.go
@@ -325,7 +325,7 @@ func (r *rpcClient) Call(ctx context.Context, request Request, response interfac
 		}
 
 		now := time.Now()
-		log.Infof(" |RCP_REQ |trace=%s |service=%s |method=%s |metadata=%s "+
+		log.Infof(" |RPC_REQ |trace=%s |service=%s |method=%s |metadata=%s "+
 			"|time=%v |address=%s |port=%d |content_type=%s |request=%v",
 			md["X-Trace-Id"],
 			node.Id,
@@ -340,7 +340,7 @@ func (r *rpcClient) Call(ctx context.Context, request Request, response interfac
 		// make the call
 		err = rcall(ctx, address, request, response, callOpts)
 		r.opts.Selector.Mark(request.Service(), node, err)
-		log.Infof(" |RCP_RSP |duration=%v |trace=%s |service=%s |method=%s |metadata=%s "+
+		log.Infof(" |RPC_RSP |duration=%v |trace=%s |service=%s |method=%s |metadata=%s "+
 			"|time=%v |address=%s |port=%d |content_type=%s |response=%v |err=%v",
 			time.Since(now),
 			md["X-Trace-Id"],
